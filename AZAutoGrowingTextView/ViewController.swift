1c101c241b148270e666ccd362acad25d982aa38
Ashiq Uz Zoha <ashiq.ayon@gmail.com>
2016-05-27 20:55:00 +0600

    Initial Commit

---
+++ AZAutoGrowingTextView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AZAutoGrowingTextView
+//
+//  Created by Ashiq Uz Zoha on 5/27/16.
+//  Copyright © 2016 Dhrubok Infotech Services Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
